db03627235c79533c78aeef3a53cfa86a65634a5
ZaugolnovAleksander <zausasha@gmail.com>
2023-06-07 10:39:29 +0300

    M3E1-15-1-1-1 Add sout Hello

--- src/main/java/com/javarush/Main.java
+++ src/main/java/com/javarush/Main.java
@@ -2,6 +2,10 @@ package com.javarush;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        System.out.println(" hello from MAIN");
+
     }
 }
